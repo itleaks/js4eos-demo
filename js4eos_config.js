0e77597e3ca706253020d894e3edf8ce53ec21a6
Jack Itleaks <itleaks@126.com>
2018-12-17 15:03:10 +0800

    Old HttpEndpoint in kylin unavailable,Change a new one
    Signed-off-by: Jack Itleaks <itleaks@126.com>

--- js4eos_config.js
+++ js4eos_config.js
@@ -12,7 +12,7 @@ module.exports = {
         },
         kylin: {
             chainId:'5fff1dae8dc8e2fc4d5b23b2c7665c97f9e9d8edf2b6485a86ba311c25639191',
-            httpEndpoint:'http://api.kylin.eosbeijing.one:8880',
+            httpEndpoint:'http://api.kylin.eoseco.com', //change to another httpEndpoint if this is unavailable
             keyPrefix:'EOS',
         },
     },
@@ -29,6 +29,6 @@ module.exports = {
     keyProvider:[
 		'5JnzYUtJMGhnC4kzU5RTQza8EbaX1LHm4CGXoM3ppm1RBunKUvN',//replace private key to  your contract account
 	],
-    defaultNetwork:'kylin',
+    defaultNetwork:'kylin', //change to a eos network(jungle, mainnet) which you are working on
     eosio_cdt:false,
 }
--- test/table-demo.js
+++ test/table-demo.js
@@ -6,16 +6,24 @@ var options = {
 }
 
 Js4eos = require("js4eos")(options)
-async function test() {
-    let system = await Js4eos.Contract("eosio")
-    // table(tablename, scope, lower, upper, limit_count, key_type, index_position)
-    // index <= upper and index >= lower limit limit_count
-    // primary index is 1
-    let data = await system.table("producers", "eosio", "aiguoniceman", "atticlabckbp", 5, "name", 1)
-    console.log("between", data.rows.length, data)
-    data = await system.table("producers", "eosio", "aiguoniceman", "", 5, "name", 1)
-    console.log("large than", data.rows.length, data)
-    data = await system.table("producers", "eosio", "", "atticlabckbp", 5, "name", 1)
-    console.log("less than", data.rows.length, data)
-}
-test()
+
+describe("eos table test", function() {
+    //Must set a large timeout
+    this.timeout(10000);
+    var system;
+    before(async function(){
+        system = await Js4eos.Contract("eosio") //use contract account to import contract
+    });
+
+    it('Test producers table in system contract', async function(){
+        // table(tablename, scope, lower, upper, limit_count, key_type, index_position)
+        // index <= upper and index >= lower limit limit_count
+        // primary index is 1
+        let data = await system.table("producers", "eosio", "aiguoniceman", "atticlabckbp", 5, "name", 1)
+        console.log("between", data.rows.length, data)
+        data = await system.table("producers", "eosio", "aiguoniceman", "", 5, "name", 1)
+        console.log("large than", data.rows.length, data)
+        data = await system.table("producers", "eosio", "", "atticlabckbp", 5, "name", 1)
+        console.log("less than", data.rows.length, data)
+    });
+});
